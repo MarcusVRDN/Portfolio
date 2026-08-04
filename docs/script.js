cec7136157b86ca8fcfe16d393986f3f31f00b69
Marcus Nascimento <marcusvinirdn@gmail.com>
2026-08-04 10:24:57 -0300

    exclui translation.js

--- docs/script.js
+++ docs/script.js
@@ -63,7 +63,7 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
       if (entry.isIntersecting) {
     entry.target.classList.add("visible")
-  } 
+  } {threshold: 0.2} 
   })
 
 }, {threshold: 0.2} );
